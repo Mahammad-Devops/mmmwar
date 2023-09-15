aeca16f4d8f16eca9688083cb75c521aff257347
Mahammad-Devops <meerabinginipalli@gmail.com>
2023-09-15 14:44:51 +0530

    v1

---
+++ src/test/java/test10/sammethodtest.java
@@ -0,0 +1,14 @@
+package test10;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class sammethodtest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
